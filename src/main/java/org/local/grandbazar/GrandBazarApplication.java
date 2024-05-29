a4255b65e92726e3795d4a1a1330316106e8a48b
benji_mac <benaja.bendo02@gmail.com>
2024-05-29 13:25:13 +0200

    premier commit

---
+++ src/main/java/org/local/grandbazar/GrandBazarApplication.java
@@ -0,0 +1,21 @@
+package org.local.grandbazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping
+public class GrandBazarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrandBazarApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Welcome to Grand Bazar!";
+    }
+}
---
+++ src/test/java/org/local/grandbazar/GrandBazarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.local.grandbazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandBazarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
