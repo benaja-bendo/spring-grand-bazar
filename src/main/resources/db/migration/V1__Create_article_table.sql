eb7fa448c64f3de920cc7bed4d322286c5f1b75e
Mourtada <mourtadam21@gmail.com>
2024-05-30 13:54:58 +0200

    modifs

--- src/main/resources/db/migration/V1__Create_article_table.sql
+++ src/main/resources/db/migration/V1__Create_article_table.sql
@@ -1,6 +1,7 @@
-CREATE TABLE article
-(
-    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title   VARCHAR(255) NOT NULL,
-    content TEXT         NOT NULL
+CREATE TABLE article (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  content TEXT NOT NULL,
+  vendeur_id BIGINT NOT NULL,
+  CONSTRAINT fk_article_vendeur FOREIGN KEY (vendeur_id) REFERENCES vendeur(id)
 );
